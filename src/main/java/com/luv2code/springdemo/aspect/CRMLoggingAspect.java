03e77735e31676d489e7ad89d7f25662e10a6149
fraczekkrzysztof <fraczekkrzysztof@o2.pl>
2018-04-23 19:46:24 +0200

    AOP
    Add @AfterReturning aspect

--- src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -43,11 +44,27 @@ public class CRMLoggingAspect {
 		myLogger.info("=======> in @Before: calling method: " + theMethod);
 		
 		//display the arguments to the method
+		//get the arguments
+		Object[] arg = theJoinPoint.getArgs();
 		
+		//loop thru and display arg
+		for (Object tempArg: arg) {
+			myLogger.info("=======> argument: "+ tempArg);
+		}
 	}
-	
-	
-	
-	
+		
 	//add @AdterReturning advice
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		//display method we are returning from
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=======> in @AfterReturning: from method: " + theMethod);
+		
+		//display data returned
+		myLogger.info("=======> result: "+ theResult);
+	}
 }
